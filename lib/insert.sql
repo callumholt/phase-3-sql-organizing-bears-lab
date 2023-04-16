2bb9c012139f2300661263e3d83483d58542a3d4
callum <callumpholt@gmail.com>
2023-04-16 10:10:59 +1000

    commit tree 98e1bb112d4fcdaacca087b5fda846e079ee7760

---
+++ lib/insert.sql
@@ -0,0 +1,20 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES
+
+('Mr. Chocolate',20,'M','black','happy',1),
+('Rowdy',2,'F','white','happy',1),
+('Tabitha',3,'M','blue','sad',0),
+('Sergeant Brown',4,'M','yellow','excited',0),
+('Melissa',5,'M','purple','pumped',0),
+('Grinch',6,'F','black','happy',1),
+('Wendy',7,'F','white','sad',1),
+(NULL,6,'F','golden','happiest',1);
+
+
+
+
+
+
+
+
+
+
